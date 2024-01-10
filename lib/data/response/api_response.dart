2f5ec8fccadc94f1b0179defc6a5ba6c94939bd5
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-10 10:59:28 +0500

    In this api_responcefile i defined different method for api responces

---
+++ lib/data/response/api_response.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:mvvmprac/data/response/status.dart';
+
+class ApiResponse<T> {
+  Status? status;
+  T? data;
+  String? message;
+
+  ApiResponse(this.status , this.data , this.message);
+
+  ApiResponse.loading() : status = Status.LOADING;
+  ApiResponse.completed(this.data) : status = Status.COMPLETED;
+  ApiResponse.error(this.message) : status = Status.ERROR;
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "Status : $status \n Message $message : Data : $data";
+  }
+
+}
