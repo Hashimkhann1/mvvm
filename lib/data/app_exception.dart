349bff89cb52cbe200f8f0d462dee20a4972626f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-11 15:06:43 +0500

    new exceptions is addd to this file

--- lib/data/app_exception.dart
+++ lib/data/app_exception.dart
@@ -24,3 +24,15 @@ class ServerException extends AppException{
   ServerException([String? message]) : super(message, "Internal Server Exception");
 }
 
+class BadRequestException extends AppException{
+  BadRequestException([String? message]) : super(message, "Invalid or Bad Request");
+}
+
+class DataNotFoundException extends AppException{
+  DataNotFoundException([String? message]) : super(message, "404 Not Found");
+}
+
+class ApiException extends AppException{
+  ApiException([String? message]) : super(message, "Unexpected error occurred");
+}
+
