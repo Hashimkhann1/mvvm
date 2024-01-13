9922f18b00c53678d51666c324a7bd5b1ce0c95e
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-13 22:07:14 +0500

    login view routes is added

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -1,14 +1,22 @@
 import 'package:get/get.dart';
 import 'package:mvvmprac/res/routes/routes_name.dart';
+import 'package:mvvmprac/view/login_view/login_view.dart';
 
 import '../../view/splash_view.dart';
 
 class AppRoutes {
   static appRoutes() => [
-        GetPage(
+    GetPage(
             name: RoutesName.splashView,
             page: () => const SplashView(),
             transitionDuration: const Duration(microseconds: 260),
             transition: Transition.leftToRight),
+
+    // lgon view route
+    GetPage(
+        name: RoutesName.loginView,
+        page: () => const LoginView(),
+        transitionDuration: const Duration(microseconds: 260),
+        transition: Transition.leftToRight),
       ];
 }
