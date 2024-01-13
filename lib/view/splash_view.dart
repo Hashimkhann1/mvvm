e69c6e26a8c7f17a9a190cf7aa882e50c9275a01
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-13 22:08:19 +0500

    function is add for splash time

--- lib/view/splash_view.dart
+++ lib/view/splash_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mvvmprac/data/app_exception.dart';
+import 'package:mvvmprac/res/assets/images_assets.dart';
+import 'package:mvvmprac/view_model/splash_services/splash_services.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -9,17 +11,31 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+
+  final ImagesAssets imagesAssets  = ImagesAssets();
+  final SplashServices splashServices = SplashServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    splashServices.isLogin();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
       body: Center(
-        child: Text("Spash View"),
+        child: Text("Welcome back")
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          throw InternetException("! Your internat connection is not working");
-        },
-      ),
+
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     throw InternetException("! Your internat connection is not working");
+      //   },
+      // ),
     );
   }
 }
