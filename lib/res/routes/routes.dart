ec7506938eafeac9074bf41692ad7f11cf1a093b
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-14 15:22:20 +0500

    home view routes is added

--- lib/res/routes/routes.dart
+++ lib/res/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:mvvmprac/res/routes/routes_name.dart';
+import 'package:mvvmprac/view/home_view/home_view.dart';
 import 'package:mvvmprac/view/login_view/login_view.dart';
 
 import '../../view/splash_view.dart';
@@ -18,5 +19,13 @@ class AppRoutes {
         page: () => const LoginView(),
         transitionDuration: const Duration(microseconds: 260),
         transition: Transition.leftToRight),
+
+    // home view
+    GetPage(
+        name: RoutesName.homeView,
+        page: () => const HomeView(),
+        transitionDuration: const Duration(microseconds: 260),
+        transition: Transition.leftToRight),
+
       ];
 }
