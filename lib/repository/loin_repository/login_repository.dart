2d9cac668c4e3a480fc4d2a8d381a4353027cc42
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-14 15:39:58 +0500

    calling to postApi function

--- lib/repository/loin_repository/login_repository.dart
+++ lib/repository/loin_repository/login_repository.dart
@@ -2,9 +2,15 @@
 
 
 import 'package:mvvmprac/data/network/network_api_services.dart';
+import 'package:mvvmprac/res/app_urls/app_urls.dart';
 
-class LoinRepository {
+class LoginRepository {
 
   final _apiServices = NetworkApiServics();
+
+  Future<dynamic> loinApi(var data, ) async {
+    dynamic responce = _apiServices.postApi(data, AppUrls.loginApi);
+    return responce;
+  }
 
 }
