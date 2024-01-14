2adc16212a08df8574da56cf5f5dc79f2a439365
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-14 15:30:03 +0500

    In this file i write functions for saving loedin user token and removing user token for sign out

--- lib/view_model/user_preference_view_model/user_preference_view_model.dart
+++ lib/view_model/user_preference_view_model/user_preference_view_model.dart
@@ -1,2 +1,32 @@
 
 
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:mvvmprac/model/models/login/login_responce_model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserPreference {
+
+
+  Future<bool> saveUser(UserModel loginResponceModel) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setString('token', loginResponceModel.token.toString());
+    return true;
+  }
+
+  Future<UserModel> getUser() async {
+
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String? token = sharedPreferences.getString('token');
+
+    return UserModel(
+      token: token
+    );
+  }
+
+  Future<bool> removeUser() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.clear();
+    return true;
+  }
+
+}
