d90222f7eb9c5ab1932e7087f99c5b065049ce26
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-14 15:22:51 +0500

    home view routes name is added

--- lib/res/routes/routes_name.dart
+++ lib/res/routes/routes_name.dart
@@ -4,6 +4,7 @@ class RoutesName {
 
   static const String splashView = '/';
   static const String loginView = '/login_view';
+  static const String homeView = '/home_view';
   // static const String loginView = 'splash_view';
 
 }
