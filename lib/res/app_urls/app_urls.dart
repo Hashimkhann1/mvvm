f7cf1380dfe23fcbae4716a7e423b2b1f82dfe58
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-01-17 22:00:33 +0500

    user data api url path is added

--- lib/res/app_urls/app_urls.dart
+++ lib/res/app_urls/app_urls.dart
@@ -6,4 +6,6 @@ class AppUrls {
   static const String baseUrl = 'https://reqres.in';
   static const String loginApi = '/api/login';
 
+  static const String userListApi = 'https://webhook.site/32704056-4285-4013-882c-e0a4c938a6b5';
+
 }
